3b0b4be6150e1418cfaaea6005412036797888b5
Ilyas Dzhalilov <dzhalilov.ilyas@gmail.com>
2022-05-12 18:37:17 +0300

    Init

---
+++ src/main/java/com/example/chessvolgograd/ChessVolgogradApplication.java
@@ -0,0 +1,13 @@
+package com.example.chessvolgograd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessVolgogradApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChessVolgogradApplication.class, args);
+    }
+
+}
