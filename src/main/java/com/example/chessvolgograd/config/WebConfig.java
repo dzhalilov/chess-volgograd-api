426162251dc43d8feb6e904f616e0ffd6f0098d7
Ilyas Dzhalilov <dzhalilov.ilyas@gmail.com>
2023-04-15 22:25:09 +0300

    add Swagger change spring-boot from 2.6.7 to 2.5.6 for fix Swagger

--- src/main/java/com/example/chessvolgograd/config/WebConfig.java
+++ src/main/java/com/example/chessvolgograd/config/WebConfig.java
@@ -41,6 +41,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+        registry.addResourceHandler("swagger-ui.html")
+            .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+            .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     @Override
