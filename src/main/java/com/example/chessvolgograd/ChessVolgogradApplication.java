1d63b4ad41f64c381187ba0f3970094c40e53c4d
Ilyas Dzhalilov <dzhalilov.ilyas@gmail.com>
2023-04-17 23:38:53 +0300

    move @EnableSwagger2 to ChessVolgogradApplication and delete SpringFoxConfig.java

--- src/main/java/com/example/chessvolgograd/ChessVolgogradApplication.java
+++ src/main/java/com/example/chessvolgograd/ChessVolgogradApplication.java
@@ -4,9 +4,11 @@ import com.example.chessvolgograd.config.AppConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppConfig.class)
+@EnableSwagger2
 public class ChessVolgogradApplication {
 
   public static void main(String[] args) {
